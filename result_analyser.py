adf92152a2277c2f49cc5aebd1b26b3aa1c7bef0
Chenfa Zheng <zhengcf@mail.bnu.edu.cn>
2022-06-25 03:04:06 +0800

    additional check if obsid matches in result files

--- result_analyser.py
+++ result_analyser.py
@@ -16,6 +16,27 @@ if __name__ == '__main__':
     corr_list_path = os.path.join(work_dir, 'results_corr.txt')
     corr_dict = read_list_file(corr_list_path)
 
+    # mask if the obsid not match among three files
+    obsid_arr = res_arr['obsid']
+    obsid_mask = []
+    for obsid in obsid_arr:
+        if obsid in v_dict.keys() and obsid in corr_dict.keys():
+            obsid_mask.append(True)
+        else:
+            obsid_mask.append(False)
+    obsid_mask = np.array(obsid_mask)
+    res_arr = res_arr[obsid_mask]
+    obsid_arr = obsid_arr[obsid_mask]
+
+    # drop masked data in dict
+    v_dict = {k: v for k, v in v_dict.items() if k in obsid_arr}
+    corr_dict = {k: v for k, v in corr_dict.items() if k in obsid_arr}
+
+    # report mask status
+    total_count = len(obsid_mask)
+    mask_count = len(obsid_mask) - np.sum(obsid_mask)
+    print('Total count: {}, Masked count: {}'.format(total_count, mask_count))
+
     # plot
     n_bins = 100
 
@@ -42,6 +63,18 @@ if __name__ == '__main__':
     ax.set_title('Histogram of v_mean - v_lamost')
     plt.tight_layout()
     fig_save_path = os.path.join(work_dir, 'v_mean_v_lamost_hist.png')
+    fig.savefig(fig_save_path, dpi=300)
+    plt.close()
+
+    # histogram of v_lamost
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.hist(v_lamost_arr, bins=n_bins, density=True)
+    ax.set_xlabel('v_lamost (km/s)')
+    ax.set_ylabel('density')
+    ax.set_title('Histogram of v_lamost')
+    plt.tight_layout()
+    fig_save_path = os.path.join(work_dir, 'v_lamost_hist.png')
     fig.savefig(fig_save_path, dpi=300)
     plt.close()
 
@@ -207,5 +240,54 @@ if __name__ == '__main__':
     fig.suptitle('Histogram of v - v_lamost for all segments [-50, 50]')
     plt.tight_layout()
     fig_save_path = os.path.join(work_dir, 'v_v_lamost_seg_hist_50.png')
+    fig.savefig(fig_save_path, dpi=300)
+    plt.close()
+
+    # histogram of v for all segments
+    fig = plt.figure(figsize=(6, 10))
+    gs = fig.add_gridspec(nrows, hspace=0)
+    axs = gs.subplots(sharex=True)
+    for i, ax in enumerate(axs):    # i for each segment
+        vs = []
+        for j, obsid in enumerate(obsid_arr):   # j for each obsid
+            v_this = v_dict[obsid][i]
+            vs.append(v_this)
+        vs = np.array(vs)
+        ax.hist(vs, bins=n_bins, density=True, label=seg_labels[i])
+        ax.legend(loc='upper right')
+        if i == 0:
+            ax.set_ylabel('density')
+        if i == nrows - 1:
+            ax.set_xlabel('v (km/s)')
+        ax.axvline(0, color='k', linestyle='--', linewidth=0.5, alpha=0.8)
+    fig.suptitle('Histogram of v for all segments')
+    plt.tight_layout()
+    fig_save_path = os.path.join(work_dir, 'v_seg_hist.png')
+    fig.savefig(fig_save_path, dpi=300)
+    plt.close()
+
+    # histogram of v for all segments in [-200, 200]
+    fig = plt.figure(figsize=(6, 10))
+    gs = fig.add_gridspec(nrows, hspace=0)
+    axs = gs.subplots(sharex=True)
+    for i, ax in enumerate(axs):    # i for each segment
+        vs = []
+        for j, obsid in enumerate(obsid_arr):   # j for each obsid
+            v_this = v_dict[obsid][i]
+            if v_this < -200 or v_this > 200:
+                continue
+            vs.append(v_this)
+        vs = np.array(vs)
+        ax.hist(vs, bins=n_bins, density=True, label=seg_labels[i])
+        ax.legend(loc='upper right')
+        if i == 0:
+            ax.set_ylabel('density')
+        if i == nrows - 1:
+            ax.set_xlabel('v (km/s)')
+        ax.axvline(0, color='k', linestyle='--', linewidth=0.5, alpha=0.8)
+        ax.set_xlim([-200, 200])
+    fig.suptitle('Histogram of v for all segments [-200, 200]')
+    plt.tight_layout()
+    fig_save_path = os.path.join(work_dir, 'v_seg_hist_200.png')
     fig.savefig(fig_save_path, dpi=300)
     plt.close()
